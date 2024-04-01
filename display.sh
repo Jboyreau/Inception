7bf9f3bd2a63c8949c906d647a0f68c5e20873b6
Jacques Boyreau <jacques.boyreau94@gmail.com>
2024-04-01 11:24:40 +0200

    WP-CLI added

---
+++ display.sh
@@ -0,0 +1,30 @@
+echo ~srcs/.env;
+cat srcs/.env;
+
+echo ~srcs/docker-compose.yml;
+cat srcs/docker-compose.yml;
+
+echo ~srcs/requirements/wordpress/Dockerfile;
+cat srcs/requirements/wordpress/Dockerfile;
+
+echo ~srcs/requirements/wordpress/entrypoint.sh;
+cat srcs/requirements/wordpress/entrypoint.sh;
+
+echo ~srcs/requirements/nginx/Dockerfile;
+cat srcs/requirements/nginx/Dockerfile;
+
+echo ~srcs/requirements/nginx/conf/nginx.conf;
+cat srcs/requirements/nginx/conf/nginx.conf;
+
+echo ~srcs/requirements/mariadb/Dockerfile;
+cat srcs/requirements/mariadb/Dockerfile;
+
+echo ~srcs/requirements/mariadb/entrypoint.sh;
+cat srcs/requirements/mariadb/entrypoint.sh;
+
+echo ~srcs/requirements/mariadb/conf/init-db.sql;
+cat srcs/requirements/mariadb/conf/init-db.sql;
+
+echo ~srcs/requirements/mariadb/conf/my.cnf;
+cat srcs/requirements/mariadb/conf/my.cnf;
+
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 set -e
 
-# Vérifie si WordPress est déjà installé
+# Attendez que MariaDB soit prête
+until mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
+    echo 'En attente de MariaDB...'
+    sleep 1
+done
+
+# Vérifie si WordPress est déjà installé en vérifiant wp-settings.php et avec wp-cli
 if [ ! -f "/var/www/html/wp-settings.php" ]; then
-    echo "WordPress non trouvé dans /var/www/html, installation..."
+    echo "WordPress non trouvé dans /var/www/html, téléchargement de WordPress..."
     # Télécharge et extrait WordPress
     curl -o wordpress.tar.gz -SL "https://wordpress.org/latest.tar.gz" 
     tar -xzf wordpress.tar.gz -C /var/www/html --strip-components=1
     rm wordpress.tar.gz
     chown -R www-data:www-data /var/www/html
+fi
+
+if ! wp core is-installed --allow-root --path='/var/www/html'; then
+    echo "Configuration de WordPress..."
+    # Créez un fichier wp-config.php avec les détails de la base de données
+    wp config create \
+        --dbname="$WORDPRESS_DB_NAME" \
+        --dbuser="$WORDPRESS_DB_USER" \
+        --dbpass="$WORDPRESS_DB_PASSWORD" \
+        --dbhost="$WORDPRESS_DB_HOST" \
+        --allow-root \
+        --path='/var/www/html'
+
+    # Installe WordPress avec les informations d'admin spécifiées dans les variables d'environnement
+    wp core install \
+        --url="$DOMAIN_NAME" \
+        --title="Exemple de WordPress" \
+        --admin_user="$WORDPRESS_ADMIN_USER" \
+        --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+        --admin_email="$WORDPRESS_ADMIN_EMAIL" \
+        --allow-root \
+        --path='/var/www/html'
+    # Configurations supplémentaires (thèmes, plugins, etc.) peuvent être ajoutées ici
 fi
 
 # Continue avec la commande originale
