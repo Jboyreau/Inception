127ef707e334c2b2e956a457b8c0a9f4977dfabc
Jacques Boyreau <jacques.boyreau94@gmail.com>
2024-04-01 12:48:02 +0200

    wordpress fix

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -7,19 +7,18 @@ until mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
     sleep 1
 done
 
-# Vérifie si WordPress est déjà installé en vérifiant wp-settings.php et avec wp-cli
+# Télécharge et prépare WordPress si non installé
 if [ ! -f "/var/www/html/wp-settings.php" ]; then
-    echo "WordPress non trouvé dans /var/www/html, téléchargement de WordPress..."
-    # Télécharge et extrait WordPress
-    curl -o wordpress.tar.gz -SL "https://wordpress.org/latest.tar.gz" 
+    echo "WordPress non trouvé dans /var/www/html, téléchargement et extraction..."
+    curl -o wordpress.tar.gz -SL "https://wordpress.org/latest.tar.gz"
     tar -xzf wordpress.tar.gz -C /var/www/html --strip-components=1
     rm wordpress.tar.gz
     chown -R www-data:www-data /var/www/html
 fi
 
-if ! wp core is-installed --allow-root --path='/var/www/html'; then
+# Configure WordPress si wp-config.php n'existe pas
+if [ ! -f "/var/www/html/wp-config.php" ]; then
     echo "Configuration de WordPress..."
-    # Créez un fichier wp-config.php avec les détails de la base de données
     wp config create \
         --dbname="$WORDPRESS_DB_NAME" \
         --dbuser="$WORDPRESS_DB_USER" \
@@ -27,17 +26,18 @@ if ! wp core is-installed --allow-root --path='/var/www/html'; then
         --dbhost="$WORDPRESS_DB_HOST" \
         --allow-root \
         --path='/var/www/html'
+fi
 
-    # Installe WordPress avec les informations d'admin spécifiées dans les variables d'environnement
+# Installe WordPress si pas déjà installé
+if ! wp core is-installed --allow-root --path='/var/www/html'; then
     wp core install \
         --url="$DOMAIN_NAME" \
-        --title="Exemple de WordPress" \
+        --title="Exemple WordPress" \
         --admin_user="$WORDPRESS_ADMIN_USER" \
         --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
         --admin_email="$WORDPRESS_ADMIN_EMAIL" \
         --allow-root \
         --path='/var/www/html'
-    # Configurations supplémentaires (thèmes, plugins, etc.) peuvent être ajoutées ici
 fi
 
 # Continue avec la commande originale
