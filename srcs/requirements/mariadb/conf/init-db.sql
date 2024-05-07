2f85ddc01a6b4f64582e08a25c7e765fb1b37805
Jacques Boyreau <jacques.boyreau94@gmail.com>
2024-05-07 02:19:18 +0200

    end

--- srcs/requirements/mariadb/conf/init-db.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'jboyreau'@'%' IDENTIFIED BY 'azerty';
-FLUSH PRIVILEGES;
-
--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 set -e
 
-# Initialisation de la base de données si le répertoire /var/lib/mysql est vide
 if [ -z "$(ls -A /var/lib/mysql)" ]; then
     echo "Initialisation de la base de données..."
     mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
-    # Démarrage temporaire de MariaDB
-    mysqld_safe --nowatch
+    mysqld_safe --nowatch &
 
-    # Attente de démarrage de MariaDB
     until mysqladmin ping >/dev/null 2>&1; do
-        echo "En attente de démarrage de MariaDB..."
         sleep 1
     done
 
-    # Exécution des scripts d'initialisation
-    if [ -d /docker-entrypoint-initdb.d ]; then
-        echo "Exécution des scripts d'initialisation..."
-        for script in /docker-entrypoint-initdb.d/*.sql; do
-            echo "Exécution de $script"
-            mysql < "$script"
-        done
-    fi
-
-    # Arrêt de MariaDB après initialisation
-    mysqladmin shutdown
+    # Utilisation de la nouvelle syntaxe pour définir le mot de passe
+    mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+    mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+    mysql -e "CREATE USER IF NOT EXISTS \`jboyreau\`@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`jboyreau\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -e "REVOKE ALL PRIVILEGES, GRANT OPTION FROM ''@'localhost';"  # Révoquer les privilèges pour les utilisateurs sans mot de passe
+    mysql -e "REVOKE ALL PRIVILEGES, GRANT OPTION FROM ''@'%';"  # Révoquer les privilèges pour les utilisateurs sans mot de passe    mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 fi
 
-# Démarrage normal de MariaDB
 exec "$@"
+
 
